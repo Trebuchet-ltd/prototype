8225a28e6e710d95f9ffd84c66714e3bf90bb238
sathrij <sahilathrij@gmail.com>
2021-07-22 02:18:45 +0530

    auto add cart to user

--- prototype/settings.py
+++ prototype/settings.py
@@ -161,10 +161,10 @@ else:
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 MEDIA_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
