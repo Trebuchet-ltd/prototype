0c605384611c9be5cf95daaccdb3925a8423b42c
sathrij <sahilathrij@gmail.com>
2021-07-24 11:00:25 +0530

    fixing auth.me

--- prototype/settings.py
+++ prototype/settings.py
@@ -110,8 +110,12 @@ WSGI_APPLICATION = 'prototype.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR / 'db.sqlite3')),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
