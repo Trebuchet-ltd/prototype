105e113eaee1a9f2b256e5b6aac7bfe1e2100bea
sunith <sunithvs2002@gmail.com>
2022-01-03 02:39:08 +0530

    added gst to seri

--- billing/utils.py
+++ billing/utils.py
@@ -5,10 +5,6 @@ from django.contrib.auth.models import User
 from django.db.models import Sum
 
 from home.models import Orders, TransactionDetails, OrderItem, Addresses
-from .models import Book
-from .models import BookLog
-from .models import Inventory
-from .models import InventoryLog
 from home.models import Product
 
 
@@ -65,120 +61,11 @@ def invoice_data_processor(invoice_post_data):
                 amt_with_tax = item.price * weight * (1 + item.product_gst_percentage / 100)
                 transaction.total += amt_with_tax * (1 - item.discount / 100)
                 transaction.invoice_amt_without_gst = item.price * weight * (1 - item.discount / 100)
-
+                item.stock -= int(weight * quantity/1000)
+                item.save()
                 is_cleaned = True
                 OrderItem.objects.create(item=item, quantity=quantity, weight_variants=weight, is_cleaned=is_cleaned,
                                          order=order)
             except Product.DoesNotExist:
                 pass
-
-    print(processed_invoice_data)
     return order
-
-
-def update_products_from_invoice(invoice_data_processed, request):
-    for item in invoice_data_processed['items']:
-        new_product = False
-        if Product.objects.filter(user=request.user,
-                                  product_name=item['invoice_product'],
-                                  product_hsn=item['invoice_hsn'],
-                                  product_unit=item['invoice_unit'],
-                                  product_gst_percentage=item['invoice_gst_percentage']).exists():
-            product = Product.objects.get(user=request.user,
-                                          product_name=item['invoice_product'],
-                                          product_hsn=item['invoice_hsn'],
-                                          product_unit=item['invoice_unit'],
-                                          product_gst_percentage=item['invoice_gst_percentage'])
-        else:
-            new_product = True
-            product = Product(user=request.user,
-                              product_name=item['invoice_product'],
-                              product_hsn=item['invoice_hsn'],
-                              product_unit=item['invoice_unit'],
-                              product_gst_percentage=item['invoice_gst_percentage'])
-        product.product_rate_with_gst = item['invoice_rate_with_gst']
-        product.save()
-
-        if new_product:
-            create_inventory(product)
-
-
-#  ================== Inventory methods ====================
-
-def create_inventory(product):
-    if not Inventory.objects.filter(user=product.user, product=product).exists():
-        new_inventory = Inventory(user=product.user, product=product)
-        new_inventory.save()
-
-
-def update_inventory(invoice, request):
-    invoice_data = json.loads(invoice.invoice_json)
-    for item in invoice_data['items']:
-        product = Product.objects.get(user=request.user,
-                                      product_name=item['invoice_product'],
-                                      product_hsn=item['invoice_hsn'],
-                                      product_unit=item['invoice_unit'],
-                                      product_gst_percentage=item['invoice_gst_percentage'])
-        inventory = Inventory.objects.get(user=product.user, product=product)
-        change = int(item['invoice_qty']) * (-1)
-        inventory_log = InventoryLog(user=product.user,
-                                     product=product,
-                                     date=datetime.datetime.now(),
-                                     change=change,
-                                     change_type=4,
-                                     associated_invoice=invoice,
-                                     description="Sale - Auto Deduct")
-        inventory_log.save()
-        inventory.current_stock += change
-        inventory.last_log = inventory_log
-        inventory.save()
-
-
-def remove_inventory_entries_for_invoice(invoice, user):
-    inventory_logs = InventoryLog.objects.filter(user=user,
-                                                 associated_invoice=invoice)
-    for inventory_log in inventory_logs:
-        inventory_product = inventory_log.product
-        inventory_log.delete()
-        # update the inventory total
-        inventory_obj = Inventory.objects.get(user=user, product=inventory_product)
-        recalculate_inventory_total(inventory_obj, user)
-
-
-def recalculate_inventory_total(inventory_obj, user):
-    new_total = InventoryLog.objects.filter(user=user, product=inventory_obj.product).aggregate(Sum('change'))[
-        'change__sum']
-    if not new_total:
-        new_total = 0
-    inventory_obj.current_stock = new_total
-    inventory_obj.save()
-
-
-# ================ Book methods ===========================
-
-def add_customer_book(customer):
-    # check if customer already exists
-    if Book.objects.filter(user=customer.user, customer=customer).exists():
-        return
-    book = Book(user=customer.user,
-                customer=customer)
-    book.save()
-
-
-def auto_deduct_book_from_invoice(invoice):
-    invoice_data = json.loads(invoice.invoice_json)
-
-    book = Book.objects.get(user=invoice.user, customer=invoice.invoice_customer)
-
-    book_log = BookLog(parent_book=book,
-                       date=invoice.invoice_date,
-                       change_type=1,
-                       change=(-1.0) * float(invoice_data['invoice_total_amt_with_gst']),
-                       associated_invoice=invoice,
-                       description="Purchase - Auto Deduct")
-
-    book_log.save()
-
-    book.current_balance = book.current_balance + book_log.change
-    book.last_log = book_log
-    book.save()
--- billing/views.py
+++ billing/views.py
@@ -2,57 +2,23 @@ import datetime
 
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.http import JsonResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render
 
 from home.models import Orders, OrderItem
 from home.models import Product
-from .forms import BookLogForm
-from .forms import CustomerForm
-from .forms import InventoryLogForm
 from .forms import ProductForm
-from .forms import UserProfileForm
-from .models import Book
-from .models import BookLog
-from .models import Customer
-from .models import Inventory
-from .models import InventoryLog
-from .models import Invoice
-from .models import UserProfile
-from .utils import add_customer_book
-from .utils import create_inventory
 from .utils import invoice_data_processor
 from .utils import invoice_data_validator
-from .utils import remove_inventory_entries_for_invoice
-from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 
 # User Management =====================================
-
-@login_required
-def user_profile_edit(request):
-    context = {}
-    user_profile = get_object_or_404(UserProfile, user=request.user)
-    context['user_profile_form'] = UserProfileForm(instance=user_profile)
-
-    if request.method == "POST":
-        user_profile_form = UserProfileForm(request.POST, instance=user_profile)
-        user_profile_form.save()
-        return redirect('user_profile')
-    return render(request, 'gstbillingapp/user_profile_edit.html', context)
-
-
-@login_required
-def user_profile(request):
-    context = {}
-    user_profile = get_object_or_404(UserProfile, user=request.user)
-    context['user_profile'] = user_profile
-    return render(request, 'gstbillingapp/user_profile.html', context)
 
 
 def login_view(request):
@@ -73,47 +39,14 @@ def login_view(request):
     return render(request, 'gstbillingapp/login.html', context)
 
 
-def signup_view(request):
-    if request.user.is_authenticated:
-        return redirect("invoice_create")
-    context = {}
-    signup_form = UserCreationForm()
-    profile_edit_form = UserProfileForm()
-    context["signup_form"] = signup_form
-    context["profile_edit_form"] = profile_edit_form
-
-    if request.method == "POST":
-        signup_form = UserCreationForm(request.POST)
-        profile_edit_form = UserProfileForm(request.POST)
-        context["signup_form"] = signup_form
-        context["profile_edit_form"] = profile_edit_form
-
-        if signup_form.is_valid():
-            user = signup_form.save()
-        else:
-            context["error_message"] = signup_form.errors
-            return render(request, 'gstbillingapp/signup.html', context)
-        if profile_edit_form.is_valid():
-            userprofile = profile_edit_form.save(commit=False)
-            userprofile.user = user
-            userprofile.save()
-            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            return redirect("invoice_create")
-
-    return render(request, 'gstbillingapp/signup.html', context)
-
-
-# Invoice, products and customers ===============================================
-
 @login_required
 def invoice_create(request):
     context = {}
     last_order = Orders.objects.all().order_by('-id').first()
     if last_order:
-        context['default_invoice_number'] = last_order.id +1
+        context['default_invoice_number'] = last_order.id + 1
     else:
-        context['default_invoice_number'] =  1
-
+        context['default_invoice_number'] = 1
 
     context['default_invoice_date'] = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
     if request.method == 'POST':
@@ -173,56 +106,8 @@ def products(request):
 
 
 @login_required
-def customersjson(request):
-    customers = list(Customer.objects.filter(user=request.user).values())
-    return JsonResponse(customers, safe=False)
-
-
-@login_required
-def productsjson(request):
-    products = list(Product.objects.filter(user=request.user).values())
-    return JsonResponse(products, safe=False)
-
-
-@login_required
-def customer_edit(request, customer_id):
-    customer_obj = get_object_or_404(Customer, user=request.user, id=customer_id)
-    if request.method == "POST":
-        customer_form = CustomerForm(request.POST, instance=customer_obj)
-        if customer_form.is_valid():
-            new_customer = customer_form.save()
-            return redirect('customers')
-    context = {}
-    context['customer_form'] = CustomerForm(instance=customer_obj)
-    return render(request, 'gstbillingapp/customer_edit.html', context)
-
-
-@login_required
-def customer_delete(request):
-    if request.method == "POST":
-        customer_id = request.POST["customer_id"]
-        customer_obj = get_object_or_404(Customer, user=request.user, id=customer_id)
-        customer_obj.delete()
-    return redirect('customers')
-
-
-@login_required
-def customer_add(request):
-    if request.method == "POST":
-        customer_form = CustomerForm(request.POST)
-        new_customer = customer_form.save(commit=False)
-        new_customer.user = request.user
-        new_customer.save()
-        add_customer_book(new_customer)
-        return redirect('customers')
-    context = {}
-    context['customer_form'] = CustomerForm()
-    return render(request, 'gstbillingapp/customer_edit.html', context)
-
-
-@login_required
 def product_edit(request, product_id):
-    product_obj = get_object_or_404(Product, user=request.user, id=product_id)
+    product_obj = Product.objects.get(id=product_id)
     if request.method == "POST":
         product_form = ProductForm(request.POST, instance=product_obj)
         if product_form.is_valid():
@@ -241,7 +126,6 @@ def product_add(request):
             new_product = product_form.save(commit=False)
             new_product.user = request.user
             new_product.save()
-            create_inventory(new_product)
 
             return redirect('products')
     context = {}
@@ -257,120 +141,6 @@ def product_delete(request):
         product_obj.delete()
     return redirect('products')
 
-
-# ================= Inventory Views ===========================
-@login_required
-def inventory(request):
-    context = {}
-    context['inventory_list'] = Inventory.objects.filter(user=request.user)
-    context['untracked_products'] = Product.objects.filter(user=request.user, inventory=None)
-    return render(request, 'gstbillingapp/inventory.html', context)
-
-
-@login_required
-def inventory_logs(request, inventory_id):
-    context = {}
-    inventory = get_object_or_404(Inventory, id=inventory_id, user=request.user)
-    inventory_logs = InventoryLog.objects.filter(user=request.user, product=inventory.product).order_by('-id')
-    context['inventory'] = inventory
-    context['inventory_logs'] = inventory_logs
-    return render(request, 'gstbillingapp/inventory_logs.html', context)
-
-
-@login_required
-def inventory_logs_add(request, inventory_id):
-    context = {}
-    inventory = get_object_or_404(Inventory, id=inventory_id, user=request.user)
-    inventory_logs = Inventory.objects.filter(user=request.user, product=inventory.product)
-    context['inventory'] = inventory
-    context['inventory_logs'] = inventory_logs
-    context['form'] = InventoryLogForm()
-
-    if request.method == "POST":
-        inventory_log_form = InventoryLogForm(request.POST)
-        invoice_no = request.POST["invoice_no"]
-        invoice = None
-        if invoice_no:
-            try:
-                invoice_no = int(invoice_no)
-                invoice = Invoice.objects.get(user=request.user, invoice_number=invoice_no)
-            except:
-                context['error_message'] = "Incorrect invoice number %s" % (invoice_no,)
-                return render(request, 'gstbillingapp/inventory_logs_add.html', context)
-                context['form'] = inventory_log_form
-                return render(request, 'gstbillingapp/inventory_logs_add.html', context)
-
-        inventory_log = inventory_log_form.save(commit=False)
-        inventory_log.user = request.user
-        inventory_log.product = inventory.product
-        if invoice:
-            inventory_log.associated_invoice = invoice
-        inventory_log.save()
-        inventory.current_stock = inventory.current_stock + inventory_log.change
-        inventory.last_log = inventory_log
-        inventory.save()
-        return redirect('inventory_logs', inventory.id)
-
-    return render(request, 'gstbillingapp/inventory_logs_add.html', context)
-
-
-# ===================== Book views =============================
-
-@login_required
-def books(request):
-    context = {}
-    context['book_list'] = Book.objects.filter(user=request.user)
-    return render(request, 'gstbillingapp/books.html', context)
-
-
-@login_required
-def book_logs(request, book_id):
-    context = {}
-    book = get_object_or_404(Book, id=book_id, user=request.user)
-    book_logs = BookLog.objects.filter(parent_book=book).order_by('-id')
-    context['book'] = book
-    context['book_logs'] = book_logs
-    return render(request, 'gstbillingapp/book_logs.html', context)
-
-
-@login_required
-def book_logs_add(request, book_id):
-    context = {}
-    book = get_object_or_404(Book, id=book_id, user=request.user)
-    book_logs = BookLog.objects.filter(parent_book=book)
-    context['book'] = book
-    context['book_logs'] = book_logs
-    context['form'] = BookLogForm()
-
-    if request.method == "POST":
-        book_log_form = BookLogForm(request.POST)
-        invoice_no = request.POST["invoice_no"]
-        invoice = None
-        if invoice_no:
-            try:
-                invoice_no = int(invoice_no)
-                invoice = Invoice.objects.get(user=request.user, invoice_number=invoice_no)
-            except:
-                context['error_message'] = "Incorrect invoice number %s" % (invoice_no,)
-                return render(request, 'gstbillingapp/book_logs_add.html', context)
-                context['form'] = book_log_form
-                return render(request, 'gstbillingapp/book_logs_add.html', context)
-
-        book_log = book_log_form.save(commit=False)
-        book_log.parent_book = book
-        if invoice:
-            book_log.associated_invoice = invoice
-        book_log.save()
-
-        book.current_balance = book.current_balance + book_log.change
-        book.last_log = book_log
-        book.save()
-        return redirect('book_logs', book.id)
-
-    return render(request, 'gstbillingapp/book_logs_add.html', context)
-
-
-# ================= Static Pages ==============================
 
 def landing_page(request):
     context = {}
