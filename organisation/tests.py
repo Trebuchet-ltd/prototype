49781eb6c473a4612306579e4f985674d9c6e900
sunith <sunithvs2002@gmail.com>
2022-01-16 20:39:41 +0530

    done

--- organisation/tests.py
+++ organisation/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
-
 # Create your tests here.
+import pandas
+
+from billing.models import HsnCode
+
+loc = ("HSN_SAC.xlsx")
+data = pandas.read_excel(loc)
+for i in data.values:
+    HsnCode.objects.get_or_create(code=i[0], description=i[1])
