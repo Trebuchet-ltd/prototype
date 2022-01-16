4640e0703ae4457e5168ab130e6d652ad6da5e81
sunith <sunithvs2002@gmail.com>
2022-01-16 22:09:45 +0530

    done

--- home/models.py
+++ home/models.py
@@ -64,6 +64,7 @@ class Product(models.Model):
     product_gst_percentage = models.FloatField(default=0)
     product_rate_with_gst = models.FloatField(default=0)
 
+
     def __str__(self):
         return self.title
 
@@ -196,7 +197,7 @@ class Orders(models.Model):
     status = models.CharField(max_length=10, choices=order_status, default='preparing')
     coupon = models.ForeignKey(Coupon, related_name="orders", on_delete=models.CASCADE, blank=True, null=True)
     used_points = models.IntegerField(default=0, blank=True, null=True)
-    # organisation = models.ForeignKey('organisation.Organisation', on_delete=models.CASCADE, default=1)
+    organisation = models.ForeignKey('organisation.Organisation', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.user} , date-{self.date} , status -{self.status} "
@@ -216,7 +217,7 @@ class Purchase(models.Model):
     total = models.FloatField(default=0)
     address = models.ForeignKey(Addresses, related_name="purchase", on_delete=models.CASCADE, blank=True, null=True)
     date = models.DateField(auto_now_add=True)
-    # organisation = models.ForeignKey('organisation.Organisation', on_delete=models.CASCADE,  default=1)
+    organisation = models.ForeignKey('organisation.Organisation', on_delete=models.CASCADE,  default=1)
 
     @property
     def invoice_number(self):
@@ -322,8 +323,8 @@ class Tokens(models.Model):
     first_purchase_done = models.BooleanField(default=False)
     total_points_yet = models.IntegerField(default=0)
     amount_saved = models.IntegerField(default=0)
-    # organisation = models.ForeignKey(Organisation, related_name='tokens', on_delete=models.SET_NULL, null=True,
-    #                         blank=True)
+    organisation = models.ForeignKey(Organisation, related_name='tokens', on_delete=models.SET_NULL, null=True,
+                            blank=True)
 
     def __str__(self):
         return f"{self.user} "
